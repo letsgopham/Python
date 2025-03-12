19920e32c1a14e6a76b7f568a1c322e2726c30ba
letsgopham <joon2045@gmail.com>
2025-03-13 02:21:03 +0900

    걍 심심해서 한 코딩

--- python/hello.py
+++ python/hello.py
@@ -7,6 +7,22 @@ pound = kilo * 2.20462
 print(float(pound), "파운드 입니다.")
 
 ban = float(input("반지름을 입력하십시오.: "))
-pi = 3
+pi = 3.14
 circle = ban ** 2 * pi
 print("넓이는", float(circle), "입니다.")
+
+answers = {'예', "네", "yes", "합격", "응", "넵"}
+attempts = 0
+while True:
+    junseo = input("배준서는 컴퓨터과학과 학생회 인클루드에 합격했을까요?:")
+    if junseo in answers:
+        print("정답입니다.")
+        break
+    else:
+       print("오답입니다. 다시 시도하세요.")
+       attempts += 1
+
+    if attempts >= 3:
+        print("나중에 시도하십시오.")
+        break
+       
