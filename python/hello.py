7fd0ffa9013bd9c0022acecfe26dc05eba5a9204
letsgopham <joon2045@gmail.com>
2025-03-12 19:06:10 +0900

    5th commit

--- python/hello.py
+++ python/hello.py
@@ -5,3 +5,8 @@ print(float(cm), 'cm 입니다.')
 kilo = float(input("키로그램을 입력하십시오: "))
 pound = kilo * 2.20462
 print(float(pound), "파운드 입니다.")
+
+ban = float(input("반지름을 입력하십시오.: "))
+pi = 3.14
+circle = ban * pi
+print("넓이는", float(circle), "입니다.")
