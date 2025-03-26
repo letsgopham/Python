faba9a4964020f6cfa8a91bbb4cf9caf14316633
letsgopham <joon2045@gmail.com>
2025-03-26 19:54:22 +0900

    찐찐막

--- python/test.py
+++ python/test.py
@@ -1,3 +1,5 @@
+answer, a, b, c, d, total = (0, 0, 0, "", 0, 0)
+
 answer = int(input('1. 수식 계산.  2. 두 수 사이 합 계산. :'))
 
 if answer == 2:
@@ -8,10 +10,10 @@ if answer == 2:
     print("입력하신 수 사의의 합은", total, "입니다.")
 
 
-if answer == 1:
+elif answer == 1:
     c = input('수식을 입력하세요: ')
     d = eval(c)
     print("%s 결과는 %5.1f입니다." %(c, d))
     
 else:
-    print('error!!!!\n1과 2만 입력하세요!')
+    print('error!!!!\n1 or 2만 입력하세요!')
