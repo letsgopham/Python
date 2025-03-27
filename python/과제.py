be11329a77ec2369bf289addc9bd582831468c32
letsgopham <joon2045@gmail.com>
2025-03-27 13:50:21 +0900

    3주차 과제

---
+++ python/과제.py
@@ -0,0 +1,11 @@
+select = int(input('입력 진수 결정(16,10,8.2): '))
+num = int(input('값 입력: '), select)
+num_hex = hex(num)
+num_dec = num
+num_oct = oct(num)
+num_bin = bin(num)
+
+print(f"16진수 ---> {num_hex}입니다.")
+print(f"10진수 ---> {num}입니다.")
+print(f"8진수 ---> {num_oct}입니다.")
+print(f"2진수 ---> {num_bin}입니다.")
